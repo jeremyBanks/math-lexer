ac14661bd773af983631145baffd5297cc332e00
ooronning <christopher.a.ronning@gmail.com>
2019-05-30 00:06:42 -0400

    redesign the run function to allow for None handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::str::Chars;
 use std::iter::Enumerate;
+use std::borrow::Cow;
+use std::convert::From;
 
 mod tokens;
 mod char_utils;
@@ -23,14 +25,19 @@ struct Token {
 
 struct Lexer {
     input: String,
+    arr: Vec<char>,
+    position: u64,
     line: u32,
     column: u32
 }
 
 impl Lexer {
     pub fn new(input: String) -> Self {
+        let v: Vec<char> = input.clone().chars().collect();
         Lexer {
             input: input,
+            arr: v,
+            position: 0,
             line: 0,
             column: 0
         }
@@ -39,7 +46,8 @@ impl Lexer {
     pub fn next_token(&mut self, i: u64, c: char) -> Token {
         match c {
             '(' | ')' => self.tokenize_paren(c),
-            '+' | '-' | '*' | '/' | '>' | '<' | '=' => self.tokenize_operator(i, c),
+            '>' | '<' | '=' => self.tokenize_comp_operator(i, c),
+            '+' | '-' | '*' | '/' => self.tokenize_arith_operator(i, c),
             _ => panic!("Unexpected character at line {}: {}", self.line, c)
         }
     }
@@ -52,7 +60,11 @@ impl Lexer {
         unimplemented!()
     }
 
-    fn tokenize_operator(&mut self, i: u64, c: char) -> Token {
+    fn tokenize_comp_operator(&mut self, i: u64, c: char) -> Token {
+        unimplemented!()
+    }
+
+    fn tokenize_arith_operator(&mut self, i: u64, c: char) -> Token {
         unimplemented!()
     }
 
@@ -69,15 +81,29 @@ impl Lexer {
         }
     }
 
-    pub fn run(&mut self) {
+    pub fn run(&mut self) -> Vec<Token> {
+        let mut tokens: Vec<Token> = vec![];
         let clone = self.input.clone();
-        for (i, c) in clone.chars().enumerate() {
-            if c == '\n' {
-                self.line += 1;
-            } else {
-                self.next_token(i as u64, c);
+        let mut chars = clone.chars(); 
+        let mut count = chars.clone().count();
+
+        while self.position <= count as u64 {
+            match chars.next() {
+                Some(c) => {
+                    if c == '\n' {
+                        self.position += 1;
+                    } else {
+                        tokens.push(self.next_token(self.position as u64, c));
+                        self.position += 1;
+                    }
+
+                    count -= 1;
+                },
+                None => tokens.push(Token { token_type: EndOfInput, line: self.line, column: self.column})
             }
         }
+
+        tokens
     }
 }
 

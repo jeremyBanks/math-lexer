76908636be12af4319e34251bdba63df4fe4d89b
Jeremy Banks <_@jeremy.ca>
2019-06-09 22:37:39 +0000

    m

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
-use std::env;
-use std::fs;
-
 pub fn main() {
-    let input: Rc<String> = Rc::new("test".to_string());
-    let mut chars = input.chars();
-
-    loop {
-        match chars.next() {
-            Some(c) => {
-                println!("What is the value of c, here?");
-                if c == '\n' {
-                    println!("yes");
-                }
+    let mut chars = "test".chars();
+    match chars.next() {
+        Some(c) => {
+            println!("breakpoint here and look at `c`");
+            if c == 'c' {
+                println!("c");
             }
-            None => {}
         }
+        None => {}
     }
 }

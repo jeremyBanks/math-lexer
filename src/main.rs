3fbb13b1393a7482772a4cb6c3bc6c3fe9911a23
Jeremy Banks <_@jeremy.ca>
2019-06-09 18:38:59 -0400

    minimal reproduction of compiler limitation with vs code and docker container debug config

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,12 @@
-mod lexer;
-mod tokens;
-use lexer::Lexer;
-
-use std::env;
-use std::fs;
-
 pub fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut input: String;
-
-    println!("input: {:?}", args);
-
-    if args.len() < 2 || args.len() > 3 {
-        println!("usage: lexer [--file] <filepath> | lexer <input>");
-        return;
+    let mut chars = "test".chars();
+    match chars.next() {
+        Some(c) => {
+            println!("breakpoint here and look at `c`");
+            if c == 'c' {
+                println!("c");
+            }
+        }
+        None => {}
     }
-
-    input = match args[1].as_ref() {
-        "--file" => fs::read_to_string(args[2].clone()).expect("Unable to read the requested file."),
-        _ => args[1].clone()
-    };
-
-    let mut lexer = Lexer::new(input);
-    let tokens = lexer.run();
-
-    println!("tokens: {:?}", tokens);
 }
